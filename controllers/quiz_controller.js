b54a8329eaa4be65a967f5c996b15a3ffc6fec0a
Felipe Vidal López <felvida@gmail.com>
2015-07-20 19:24:52 +0200

    autoload sqlite3

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,23 +1,33 @@
 var models = require('../models/models.js'); //fvl
-
-// GET /quizes pag.28 2a
+// Autoload - factoriza el código si ruta incluye :quizId
+exports.load = function(req, res, next, quizId) {
+	models.Quiz.find(quizId).then(
+		function(quiz) {
+			if (quiz) {
+				req.quiz = quiz;
+				next();
+			} else { next(new Error('No hallo quizId=' + quizId));}
+		}
+	).catch(function(error) { next(error);});
+};
+// GET /quizes pag.32
 exports.index = function(req, res) {
 	models.Quiz.findAll().then(function(quizes) {
-		res.render('quizes/index.ejs', { quizes: quizes});
-	})
+		res.render('quizes/index', { quizes: quizes});
+	}).catch(function (error){next(error);});
 };
-// GET /quizes/:id ahora llama show pag.27 paso 1a
+// GET /quizes/:id ahora llama show pag.32
 exports.show = function(req, res) {
 	models.Quiz.find(req.params.quizId).then(function(quiz) {
-		res.render('quizes/show', {quiz: quiz, errors: []}); // pag. 27 quiz: quiz
+		res.render('quizes/show', {quiz:req.quiz, errors: []}); // pag. 27 quiz: quiz
 	})
 };
 
-// GET /quizes/:id/answer pag.27 paso 1a
+// GET /quizes/:id/answer pag32
 exports.answer = function(req, res) {
  models.Quiz.find(req.params.quizId).then(function(quiz){
   var resultado = 'Incorrecta'; // que no distinga mayusculas
-  if ( req.query.respuesta.toLowerCase() ===quiz.respuesta.toLowerCase() ) {
+  if ( req.query.respuesta.toLowerCase() ===req.quiz.respuesta.toLowerCase() ) {
     resultado = 'Correcta';  }
   res.render('quizes/answer',{ quiz: req.quiz, respuesta: resultado });
   })
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 var express = require('express'); //fvl views/index.js
 var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res) {
 	res.render('index', { title: 'QUIZ FVL' });
 });
-//router.get('/quizes/question',quizController.question);
-//router.get('/quizes/answer',quizController.answer); 
+router.param('quizId',quizController.load); // autoload
+// defino rutas /quizes
 router.get('/quizes', quizController.index); // pag. 26 y 28
 router.get('/quizes/:quizId(\\d+)', quizController.show); //pag.27
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);//pag.27
