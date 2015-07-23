8bcf4deb2b4dcccd537450766130eaa4bd9dbe0c
Felipe Vidal López <felvida@gmail.com>
2015-07-23 19:49:05 +0200

    Quiz 13, Mod.8 ok

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,44 +1,47 @@
 ﻿console.log("quiz_controller.js>");
-var models = require('../models/models.js'); //fvl
+var models = require('../models/models.js'); // se construye en fvl2015.heroku.com
+
 // GET /quizes/new pag.4 m.8
 exports.new = function(req,res){ 
 	var quiz= models.Quiz.build( { pregunta:"APreguntar",respuesta:"Respuesta"}); 
 	console.log("new>"+quiz.pregunta);
 	res.render( 'quizes/new',{quiz:quiz, errors: []}  ); 
 };
+// GET /quizes/:id/edit
  exports.edit = function(req,res){ 
-	var quiz= req.quiz; 
+	var quiz= req.quiz; // autoload instancia quiz
 	console.log("edit>"+quiz.pregunta);
 	res.render( 'quizes/edit',{quiz:quiz, errors: []}  ); 
 };
- 
+// POST  /quizes/create
 exports.create = function(req, res){
 var quiz = models.Quiz.build( req.body.quiz );
 console.log("create>"+quiz.pregunta+":"+quiz.respuesta);// decia undefined!! por override mal
-var errors = quiz.validate();//ya que el objeto errors no tiene then(
+var errors = quiz.validate(); //ya que el objeto errors no tiene then(
 if (errors)
-{   console.log("Valida err>"); 
+ {   console.log("Valida err>"); 
 	var i=0; var errores=new Array();//se convierte en [] con la propiedad message por compatibilida con layout
 	for (var prop in errors) errores[i++]={message: errors[prop]};
-	res.render('quizes/edit', {quiz: quiz, errors: errores});
-} else { console.log("Valida ok>");
-	req.quiz // save: guarda en DB campos pregunta y respuesta 
+	res.render('quizes/new', {quiz: quiz, errors: errores});
+ } else { console.log("Valida ok>");
+	quiz // save: guarda en DB campos pregunta y respuesta 
 	.save({fields: ["pregunta", "respuesta"]})
 	.then( function(){ res.redirect('/quizes')}) ;
-}
+ }
 };
+// PUT /quizes/ID:
 exports.update = function(req, res){
 req.quiz.pregunta =  req.body.quiz.pregunta;
 req.quiz.respuesta =  req.body.quiz.respuesta;
 console.log("update>"+req.quiz.pregunta+":"+req.quiz.respuesta);
-var errors = quiz.validate();//ya que el objeto errors no tiene then(
+var errors = req.quiz.validate();//ya que segun version el objeto errors no tiene then()
 if (errors)
 {   console.log("validate err>"); 
 	var i=0; var errores=new Array();//se convierte en [] con la propiedad message por compatibilida con layout
 	for (var prop in errors) errores[i++]={message: errors[prop]};
-	res.render('quizes/new', {quiz: quiz, errors: errores});
+	res.render('quizes/edit', {quiz: quiz, errors: errores});
 } else { console.log("validate ok>");
-	quiz // save: guarda en DB campos pregunta y respuesta de quiz
+	req.quiz // save: guarda en DB campos pregunta y respuesta de quiz
 	.save({fields: ["pregunta", "respuesta"]})
 	.then( function(){ res.redirect('/quizes')}) ;
 }
